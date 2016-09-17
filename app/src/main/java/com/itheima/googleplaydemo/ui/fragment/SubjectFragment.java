900051b757ff76bd1196c101c01593d757a55a91
Leon <526247082@qq.com>
2016-09-17 10:24:05 +0800

    Build subject fragment list.

--- app/src/main/java/com/itheima/googleplaydemo/ui/fragment/SubjectFragment.java
+++ app/src/main/java/com/itheima/googleplaydemo/ui/fragment/SubjectFragment.java
@@ -1,6 +1,11 @@
 package com.itheima.googleplaydemo.ui.fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import com.itheima.googleplaydemo.R;
 
 /**
  * 创建者: Leon
@@ -12,10 +17,36 @@ public class SubjectFragment extends BaseListFragment {
 
     @Override
     protected void startLoadData() {
+        onDataLoadedSuccess();
     }
 
     @Override
     protected BaseAdapter getAdapter() {
-        return null;
+        return mBaseAdapter;
     }
+
+    private BaseAdapter mBaseAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return 30;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.list_subject_item, null);
+            }
+            return view;
+        }
+    };
 }
