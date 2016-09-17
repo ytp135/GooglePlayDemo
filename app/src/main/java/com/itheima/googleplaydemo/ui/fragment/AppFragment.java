9a7489d252b166493c5a5dc4e03d53825ce2993c
Leon <526247082@qq.com>
2016-09-17 10:09:07 +0800

    Create BaseAppListFragment.

--- app/src/main/java/com/itheima/googleplaydemo/ui/fragment/AppFragment.java
+++ app/src/main/java/com/itheima/googleplaydemo/ui/fragment/AppFragment.java
@@ -1,22 +1,15 @@
 package com.itheima.googleplaydemo.ui.fragment;
 
-import android.widget.BaseAdapter;
-
 /**
  * 创建者: Leon
  * 创建时间: 2016/9/15 13:13
  * 描述： TODO
  */
-public class AppFragment extends BaseListFragment {
+public class AppFragment extends BaseAppListFragment {
     private static final String TAG = "AppFragment";
 
     @Override
     protected void startLoadData() {
-
-    }
-
-    @Override
-    protected BaseAdapter getAdapter() {
-        return null;
+        onDataLoadedSuccess();
     }
 }
--- app/src/main/java/com/itheima/googleplaydemo/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/itheima/googleplaydemo/ui/fragment/HomeFragment.java
@@ -1,11 +1,7 @@
 package com.itheima.googleplaydemo.ui.fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 
-import com.itheima.googleplaydemo.R;
 import com.itheima.googleplaydemo.ui.widget.LoopView;
 
 /**
@@ -13,44 +9,13 @@ import com.itheima.googleplaydemo.ui.widget.LoopView;
  * 创建时间: 2016/9/15 13:13
  * 描述： TODO
  */
-public class HomeFragment extends BaseListFragment {
+public class HomeFragment extends BaseAppListFragment {
     private static final String TAG = "HomeFragment";
-
-
-    @Override
-    protected BaseAdapter getAdapter() {
-        return mBaseAdapter;
-    }
 
     @Override
     protected void startLoadData() {
         onDataLoadedSuccess();
     }
-
-    private BaseAdapter mBaseAdapter = new BaseAdapter() {
-        @Override
-        public int getCount() {
-            return 30;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            if (view == null) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.list_app_item, null);
-            }
-            return view;
-        }
-    };
 
     @Override
     protected View getHeaderView() {
