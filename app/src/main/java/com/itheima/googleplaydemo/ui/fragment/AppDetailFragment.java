5eebd324cfe087e4385d5aac65cbcbc0a4173ee4
Leon <526247082@qq.com>
2016-09-18 12:10:08 +0800

    Create app detail fragment.

---
+++ app/src/main/java/com/itheima/googleplaydemo/ui/fragment/AppDetailFragment.java
@@ -0,0 +1,21 @@
+package com.itheima.googleplaydemo.ui.fragment;
+
+import android.view.View;
+
+/**
+ * 创建者: Leon
+ * 创建时间: 2016/9/18 12:04
+ * 描述： TODO
+ */
+public class AppDetailFragment extends BaseFragment {
+    private static final String TAG = "AppDetailFragment";
+
+    @Override
+    protected void startLoadData() {
+    }
+
+    @Override
+    protected View onCreateContentView() {
+        return null;
+    }
+}
