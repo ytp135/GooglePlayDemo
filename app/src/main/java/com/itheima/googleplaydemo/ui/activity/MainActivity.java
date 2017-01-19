69e792e384e9262aabd5d1e90adf46a3144ef067
Leon <526247082@qq.com>
2017-01-19 22:47:26 +0800

    Using toolbar instead of ActionBar.

--- app/src/main/java/com/itheima/googleplaydemo/ui/activity/MainActivity.java
+++ app/src/main/java/com/itheima/googleplaydemo/ui/activity/MainActivity.java
@@ -11,8 +11,8 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -44,6 +44,8 @@ public class MainActivity extends BaseActivity {
     ViewPager mVp;
     @BindView(R.id.navigation)
     NavigationView mNavigationView;
+    @BindView(R.id.tool_bar)
+    Toolbar mToolbar;
 
     private ActionBarDrawerToggle mToggle;
 
@@ -86,22 +88,14 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("GooglePlay");//设置标题
-//        actionBar.setSubtitle("黑马程序员");//设置子标题
-//        actionBar.setDisplayShowTitleEnabled(false);// 设置菜单 标题是否可见
-//        actionBar.setIcon(R.mipmap.ic_launcher);//设置icon
-//        actionBar.setLogo(R.mipmap.ic_action_delete);//设置logo
-//        actionBar.setDisplayUseLogoEnabled(true);//设置icon/logo的优先级，true为logo优先，false为icon优先，默认false.
-//        actionBar.setDisplayShowHomeEnabled(true);//设置可以显示icon/log
-        actionBar.setDisplayHomeAsUpEnabled(true);//设置back按钮是否可见
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//设置back按钮是否可见
         initActionBarDrawerLayoutToggle();
     }
 
     private void initActionBarDrawerLayoutToggle() {
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
         mToggle.syncState();
-//        mDrawerLayout.addDrawerListener(mDrawerListener);
         mDrawerLayout.addDrawerListener(mToggle);
     }
 
@@ -114,29 +108,6 @@ public class MainActivity extends BaseActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /*    private DrawerLayout.DrawerListener mDrawerListener = new DrawerLayout.DrawerListener() {
-        @Override
-        public void onDrawerSlide(View drawerView, float slideOffset) {
-
-        }
-
-        @Override
-        public void onDrawerOpened(View drawerView) {
-
-        }
-
-        @Override
-        public void onDrawerClosed(View drawerView) {
-
-        }
-
-        @Override
-        public void onDrawerStateChanged(int newState) {
-
-        }
-    };*/
-
 
 /*    public class MyPagerAdapter extends FragmentPagerAdapter {
 
